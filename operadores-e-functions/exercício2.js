80a869750e00c036a06b498c0db09036106bddba
carlos.Eduardo02 <kohohaiedition0@gmail.com>
2022-02-28 10:17:19 -0300

    calculadora

---
+++ operadores-e-functions/exercício2.js
@@ -0,0 +1,57 @@
+const readLine = require('readline').createInterface({
+    input : process.stdin,
+    output : process.stdout
+});
+
+const validarNumeroInformado = (numero) =>{
+    const resultado = Number.parseFloat(numero);
+    if(!resultado){
+        console .log('número informado não é valido');
+    }
+    return resultado
+}
+
+const validarOperador = (operador) =>{
+    switch(operador){
+        case '+':
+        case '-':
+        case '/':
+        case '*':
+        case '%':
+            return operador;
+        default:
+            console .log('operador informado é invalido');
+            return null;
+    }
+}
+readLine.question('Favor informar um número:', (numero1) => {
+    const numeroValidado1 = validarNumeroInformado(numero1);
+
+    if(numeroValidado1){
+        readLine.question('Favor informar outro número:',(numero2) => {
+            const numeroValidado2 = validarNumeroInformado(numero2);
+
+            if(numeroValidado2){
+                readLine.question('Favor informar o operador:',(operador) => {
+                    const operadorValido = validarOperador(operador);
+                    if(operadorValido){
+                        switch(operadorValido){
+                            case '+': console .log(`operador selecionado adicao resultado ${numeroValidado1} + ${numeroValidado2} = ${numeroValidado1 + numeroValidado2}`);
+                                break;
+                            case '-' : consoloe .log(`operador selecionado subtração resultado ${numeroValidado1} - ${numeroValidado2} = ${numeroValidado1 - numeroValidado2}`);
+                                 break;
+                            case '*' : console .log(`operador selecionado multiplicação resultado ${numeroValidado1} * ${numeroValidado2} = ${numeroValidado1 * numeroValidado2}`);
+                                 break;
+                            case '/' : console .log(`operador selecionado divisão resultado ${numeroValidado1} / ${numeroValidado2} = ${numeroValidado1 / numeroValidado2}`);
+                                 break;
+                            case '%' : console .log(`operador selecionado modulo resultado ${numeroValidado1} % ${numeroValidado2} = ${numeroValidado1 % numeroValidado2}`);
+                                 break;
+                            default: break;
+
+                        }
+                    }        })
+            }
+        })
+    }
+    
+})
